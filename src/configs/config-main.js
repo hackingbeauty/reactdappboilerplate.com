9192a819c62e6ed67b20e9c0671628495327da1e
Mark Muskardin <mark.muskardin@gmail.com>
2019-04-06 16:24:21 -0700

    added configs folder

---
+++ src/configs/config-main.js
@@ -0,0 +1,8 @@
+/* Main app configs go here */
+
+export const appConfig = {
+  name: 'Proof Of Ex',
+  shortName: 'ProofOfEx',
+  description: 'A P2P Digital Asset Marketplace For Ethereum',
+  splashScreenBackground: '#ffffff'
+}
