20d38d63f268f9ae7ed5d5627cbd8e38a5b7df6b
Mark Muskardin <mark.muskardin@gmail.com>
2019-11-19 17:25:13 -0800

    changed capital A in dapp

--- src/configs/config-main.js
+++ src/configs/config-main.js
@@ -1,8 +1,8 @@
 /* Main app configs go here */
 
 export const appConfig = {
-  name: 'React DApp Boilerpate',
-  shortName: 'ReactDAppBoilerplate',
-  description: 'The Best UI Boilerplate For Ethereum DApps',
+  name: 'React Dapp Boilerpate',
+  shortName: 'ReactDappBoilerplate',
+  description: 'The Best UI Boilerplate For Ethereum Dapps',
   splashScreenBackground: '#ffffff'
 }
